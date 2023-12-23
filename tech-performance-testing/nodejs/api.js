519ae97e541f54e568b0a05387ffe7568c5d2e71
Aakash Kumar <aakash.kumar1980@yahoo.com>
2023-12-23 03:41:29 +0000

    updated

--- tech-performance-testing/nodejs/api.js
+++ tech-performance-testing/nodejs/api.js
@@ -6,9 +6,48 @@ async function apiRoutes(fastify, options) {
     return { hello: 'world' };
   });
 
-  fastify.get('/api/employee/position', async (request, reply) => {
-    const db = database.getDb();
 
+  fastify.get('/api/employee/position/regular', async (request, reply) => {
+    const db = database.getDb();
+  
+    // Initialize an object to hold max salary employees by position
+    let maxSalaryByPosition = {};
+    let hiredMap = {};
+  
+    try {
+      // Stream hired-records and build a map
+      const hiredStream = db.collection('hired-records').find().stream();
+      for await (const record of hiredStream) {
+        if (!hiredMap[record.name] || hiredMap[record.name] > record.hired) {
+          hiredMap[record.name] = record.hired;
+        }
+      }
+  
+      // Stream employee-records and process data
+      const employeeStream = db.collection('employee-records').find().stream();
+      for await (const record of employeeStream) {
+        const { name, position, salary } = record;
+        if (!maxSalaryByPosition[position] || maxSalaryByPosition[position].salary < salary) {
+          maxSalaryByPosition[position] = { name, position, salary, hired: hiredMap[name] || null };
+        }
+      }
+  
+    } catch (err) {
+      return reply.code(500).send({ error: 'Error querying database' });
+    }
+  
+    // Convert the processed data into an array
+    const resultArray = Object.values(maxSalaryByPosition);
+  
+    // Sort the array by name in ascending order
+    resultArray.sort((a, b) => a.name.localeCompare(b.name));
+  
+    return resultArray;
+  });
+  
+  fastify.get('/api/employee/position/optimized', async (request, reply) => {
+    const db = database.getDb();
+  
     let result;
     try {
       result = await db.collection('employee-records').aggregate([
@@ -17,12 +56,16 @@ async function apiRoutes(fastify, options) {
         {
           $lookup: {
             from: "hired-records",
-            localField: "maxSalaryEmployee.name",
-            foreignField: "name",
+            let: { employeeName: "$maxSalaryEmployee.name" },
+            pipeline: [
+              { $match: { $expr: { $eq: ["$name", "$$employeeName"] } } },
+              { $sort: { hired: 1 } }, // Sort by hired date, can use -1 for latest
+              { $limit: 1 } // Limit to 1 document
+            ],
             as: "hiredInfo"
           }
         },
-        { $unwind: "$hiredInfo" },
+        { $unwind: { path: "$hiredInfo", preserveNullAndEmptyArrays: true } },
         {
           $project: {
             _id: 0,
@@ -31,14 +74,17 @@ async function apiRoutes(fastify, options) {
             salary: "$maxSalaryEmployee.salary",
             hired: "$hiredInfo.hired"
           }
-        }
+        },
+        { $sort: { name: 1 } } // Sort by name in ascending order
       ]).toArray();
     } catch (err) {
       return reply.code(500).send({ error: 'Error querying database' });
     }
-
+  
     return result;
   });
+  
+  
 
 }
 
