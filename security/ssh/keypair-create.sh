f4fd7e5b91b0952442333e052cbb7659ef8b3ecb
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-01-04 00:40:11 -0700

    updated

--- security/ssh/keypair-create.sh
+++ security/ssh/keypair-create.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+
+# Define your passphrase and keys directory
+PASSPHRASE="apple@26j"
+KEYS_DIR="./keys"
+find $KEYS_DIR -type f -delete
+
 echo "creating keypair..."
-ssh-keygen -t rsa -N 'apple@26j' -f id_rsa_ec2
+ssh-keygen -t rsa -m PEM -N "$PASSPHRASE" -f $KEYS_DIR/id_rsa_ec2
 
-mv id_rsa_ec2 id_rsa_ec2.pem -f
-chmod 400 id_rsa_ec2.pem
+echo "setting permissions for the private key..."
+mv $KEYS_DIR/id_rsa_ec2 $KEYS_DIR/id_rsa_ec2.pem -f
+chmod 400 $KEYS_DIR/id_rsa_ec2.pem
 
-echo "creating public key."
-openssl rsa -in id_rsa_ec2.pem -out id_rsa_ec2-decrypted.pem -passin pass:apple@26j
+echo "creating public key from the private key..."
+openssl rsa -in $KEYS_DIR/id_rsa_ec2.pem -out $KEYS_DIR/id_rsa_ec2-decrypted.pem -passin pass:$PASSPHRASE
+
