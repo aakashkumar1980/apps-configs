19a9a3adcdc059fef67e10083d112f1e5cae727b
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-01-04 00:10:56 -0700

    updated

--- security/ssl/certificate_root_domain_apps-create.sh
+++ security/ssl/certificate_root_domain_apps-create.sh
@@ -14,7 +14,7 @@ openssl genrsa \
   4096
 
 cp subj_template.conf temp_subj.conf
-sed -i "s/{PLACEHOLDER}/Aaditya Designers Root CA/" temp_subj.conf
+sed -i "s/{URL}/Aaditya Designers Root CA/" temp_subj.conf
 openssl req \
   -new \
   -x509 \
@@ -40,7 +40,7 @@ openssl genrsa \
   4096
 
 cp subj_template.conf temp_subj.conf
-sed -i "s/{PLACEHOLDER}/Aaditya Designers Pvt. Ltd./" temp_subj.conf
+sed -i "s/{URL}/adfs.adesigners.com/" temp_subj.conf
 openssl req \
   -new \
   -sha256 \
@@ -72,6 +72,52 @@ openssl pkcs12 \
   -passout pass:$PASSPHRASE \
   -out $DOMAIN_CA_NAME.pfx
 
+
+
+#########################
+### APPS CERTIFICATES ###
+#########################
+APPS_CA_NAME="certificates/*_adesigners.com"
+echo "creating $APPS_CA_NAME Key, CSR & Certificates..."
+
+openssl genrsa \
+  -des3 \
+  -passout pass:$PASSPHRASE \
+  -out $APPS_CA_NAME.key \
+  4096
+
+cp subj_template.conf temp_subj.conf
+sed -i "s/{URL}/*.adesigners.com/" temp_subj.conf
+openssl req \
+  -new \
+  -sha256 \
+  -key $APPS_CA_NAME.key \
+  -passin pass:$PASSPHRASE \
+  -out $APPS_CA_NAME.csr \
+  -config temp_subj.conf
+rm temp_subj.conf
+
+openssl x509 \
+  -req \
+  -in $APPS_CA_NAME.csr \
+  -CAcreateserial \
+  -CA $ROOT_CA_NAME.pem \
+  -CAkey $ROOT_CA_NAME.key \
+  -passin pass:$PASSPHRASE \
+  -out $APPS_CA_NAME.crt \
+  -days 365 \
+  -sha256 \
+  -extfile cert_config.txt \
+  -extensions v3_req
+
+openssl pkcs12 \
+  -export \
+  -inkey $APPS_CA_NAME.key \
+  -passin pass:$PASSPHRASE \
+  -in $APPS_CA_NAME.crt \
+  -certfile $ROOT_CA_NAME.pem \
+  -passout pass:$PASSPHRASE \
+  -out $APPS_CA_NAME.pfx
 
 
 
