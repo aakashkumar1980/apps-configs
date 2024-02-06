2cfd9593e36b29ead08dea2b8b7765c8878e5c6e
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-02-05 22:33:42 -0700

    updated

--- environment_setup-dev/system-specs.sh
+++ environment_setup-dev/system-specs.sh
@@ -1,6 +1,9 @@
 while true; do
   clear
-  top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{printf "CPU Usage: %3.1f%%\n", 100 - $1}'
+  echo "CPU Usage by Core:"
+  mpstat -P ALL 1 1 | awk '/^[0-9]/ {printf "Core %s: %3.1f%%\n", $2, 100 - $NF}'
+  echo ""
   free | awk '/Mem:/ {printf "Total Memory: %.2f MB\nUsed Memory: %.2f MB\nMemory Usage: %3.1f%%\n", $2/1024, $3/1024, $3/$2 * 100}'
-  sleep 10
+  sleep 5
 done
+
